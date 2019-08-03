692e30a22633dc07cbbe0968c959707f67700cc8
Bikash Kadariya <kadarib@eou.edu>
2019-08-03 11:58:54 -0500

    assignment 10

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# liri-node-app
-assignment 10
+LIRI-node-app
+Language Interpretor and Recogonition Interface
+
+A NPM interpretor for Spotify, Band in Town, and OMDB requests.
+
+Installing
+Once you've cloned the repository, you'll need to install the required node packages listed in packages.json. This is done with a simple npm install command.
+
+After the packages are installed you'll be able to run 'node liri.js'
+
+Interacting with LIRI
+You may either enter in one of the commands as an argument; "concert-this", "spotify-this-song", "movie-this", or "do-what-it-says", or alternatively enter in no arguments for a interactive menu.
+
+For "concert-this", "spotify-this-song", or "movie-this" you'll also need to enter in an artist/band, song title, or movie title to search for.
+
+concert-this will run a search on bandsintown.com and list all the upcoming concerts, with their name, venue location, and the date's listed for the given artist/band.
+
+spotify-this-song will search Spotify for all songs with the given title and list their artist(s), song name, album, and a link to the song preview.
+
+movie-this will search the OMDB and give you information on the movie including it's release year, IMDB rating, country of production, language, a short plot, and the starring actors.
+
+do-what-it-says will read a random.txt file, and run the command stored in the text file.
+
+Built With
+Javascript
+Node
+Dotenv
+Inquirer
+moment
+node-spotify-api
+request
+
+/Users/bikashkadariya/Desktop/assignment10/images/Screen Shot 2019-08-02 at 5.51.02 PM.png
+/Users/bikashkadariya/Desktop/assignment10/images/Screen Shot 2019-08-03 at 10.10.01 AM.png
+/Users/bikashkadariya/Desktop/assignment10/images/Screen Shot 2019-08-03 at 10.34.00 AM.png
+/Users/bikashkadariya/Desktop/assignment10/images/Screen Shot 2019-08-03 at 11.19.43 AM.png
+/Users/bikashkadariya/Desktop/assignment10/images/Screen Shot 2019-08-03 at 11.31.04 AM.png
+
---
+++ liri.js
@@ -0,0 +1,147 @@
+//Require in the .env file that holds my private Spotify keys
+require("dotenv").config();
+
+// Declare the variables for my required dependencies 
+var keys = require("./keys");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
+
+// Declare the variables that will hold the user input
+var choice = process.argv[2];
+var value = process.argv.slice(3).join(" ");
+
+// Setting up the function for the 4 user search options
+function concertThis(value) {
+    axios.get(`https://rest.bandsintown.com/artists/${value}/events?app_id=codingbootcamp`).then(
+        function (response) {
+            const date = moment(response.data[0].datetime).format('MM/DD/YYYY');
+            console.log(`Venue name: ${response.data[0].venue.name}`);
+            console.log(`Venue location: ${response.data[0].venue.city}`);
+            console.log(`Date: ${date}`);
+            console.log(`===================================================================================`);
+        }
+    );
+};
+
+// Function to search on the Spotify API
+function spotifyThis(value) {
+    if (!value) {
+        value = "The Sign"; //default Song
+    }
+    
+ 
+    spotify.search({
+            type: 'track',
+            query: `${value}`,
+            limit: 3
+        })
+        .then(function (response) {
+
+            let a = response.tracks.items;
+        
+            a.forEach(songs => {
+                const artist = `Artist(s): ${songs.artists[0].name}`;
+                const songName = `Song name: ${songs.name}`;
+                const preview = `Preview: ${songs.preview_url}`;
+                const albumName = `Album name: ${songs.album.name}`;
+                const line = `===================================================================================`;
+                console.log(artist)
+                console.log(songName);
+                console.log(preview)
+                console.log(albumName)
+                console.log(line)
+            
+
+            });
+        });
+}
+
+// Function to search on the OMDB API
+function movieThis(value) {
+    if (!value) {
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        console.log("It's on Netflix!");
+        value = "Mr. Nobody";
+        movieThis(value);
+    } else {
+        // Then run a request with axios to the OMDB API with the movie specified and log the response
+        axios.get(`http://www.omdbapi.com/?t=${value}&y=&plot=short&apikey=trilogy`).then(
+            function (response) {
+                console.log(`Title: ${response.data.Title}`);
+                console.log(`Year: ${response.data.Year}`);
+                console.log(`IMDB rating: ${response.data.imdbRating}`);
+                console.log(`Rotten Tomatoes: ${response.data.Ratings[1].Value}`);
+                console.log(`Country: ${response.data.Country}`);
+                console.log(`Language: ${response.data.Language}`);
+                console.log(`Plot: ${response.data.Plot}`);
+                console.log(`Actors: ${response.data.Actors}`);
+                console.log(`===================================================================================`);
+            }
+        );
+    }
+};
+
+// Function to run a random search for whatever is written in our random.txt file
+function doWhatItSays() {
+    // Use fs to read the random.txt file and run the search
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+        // We will then run the search
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        let dataArr = data.split(",");
+
+        // We will then re-display the content as an array for later use.
+        let choice = dataArr[0];
+        let value = dataArr[1];
+
+        function doSearch() {
+            if (choice === "concert-this") {
+                concertThis(value);
+            } else if (choice === "spotify-this-song") {
+                spotifyThis(value);
+            } else if (choice === "movie-this") {
+                movieThis(value);
+            }
+        }
+        doSearch();
+    });
+};
+
+// Main function that runs the other functions based on user input
+function liriSearch() {
+    if (choice === "concert-this") {
+        concertThis(value);
+    } else if (choice === "spotify-this-song") {
+        if (value) {
+            spotifyThis(value);
+        } else {
+            value = "The sign by ace of base";
+            spotifyThis(value);
+        }
+    } else if (choice === "movie-this") {
+        movieThis(value);
+    } else if (choice === "do-what-it-says") {
+        doWhatItSays();
+    } else {
+        console.log(`===================================================================================`);
+        console.log(
+            `First type "node liri.js" followed by any of these commands: 
+        \n concert-this "the name of an artist who is touring"
+        \n spotify-this-song "any song title"
+        \n movie-this "any movie title"
+        \n "do-what-it-says"`);
+        console.log(`===================================================================================`);
+    }
+};
+
+// Calling the main LIRI search function to begin the app
+liriSearch();
+
